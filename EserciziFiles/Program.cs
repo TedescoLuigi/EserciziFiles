9695639f4b120d95d2bd30c02b22c041278bc6d1
TedescoLuigi <tedesco.luigi@franchettisalviani.net>
2026-04-16 10:15:00 +0200

    Aggiungere i file di progetto.

---
+++ EserciziFiles/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace eserciziFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // esercizio 1
+            string cartella = "Configurazione", riga, riga2, riga3, dataOra = DateTime.Now.ToString("dd/MM/yyyy|HH:mm:ss");
+            string percorsoFile = Path.Combine(cartella, "impostazioni.log");
+
+            if (!Directory.Exists("Configurazione"))
+            {
+                Directory.CreateDirectory("Configurazione");
+            }
+
+            using (StreamWriter sr = new StreamWriter(percorsoFile)) ;
+           
+
+            using (StreamReader sr = new StreamReader("info_sistema.txt"))
+            {
+                riga = sr.ReadLine();
+                riga2 = sr.ReadLine();
+                riga3 = sr.ReadLine();
+            }
+            using (StreamWriter sw = new StreamWriter(percorsoFile))
+            {
+                sw.WriteLine("[session_start]" + dataOra);
+                sw.WriteLine("UTENTE => "+riga);
+                sw.WriteLine("SISTEMA => "+riga2);
+                sw.WriteLine("RISOLUZIONE => " + riga3);
+                sw.WriteLine("LINGUA => IT" );
+                sw.WriteLine("LIVELLO => ADMIN");
+                sw.WriteLine("AUTOSAVE => ATTIVO ");
+
+
+
+            }
+        }
+    }
+}
